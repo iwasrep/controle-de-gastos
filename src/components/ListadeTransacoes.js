a483e82172cc802a41b5dc7b03b660d7ca5a9dce
Diogo O. Lins - iWAS! <contato@iwas.com.br>
2022-02-02 20:14:15 -0300

    Condição para mostrar lista de transações

--- src/components/ListadeTransacoes.js
+++ src/components/ListadeTransacoes.js
@@ -1,18 +1,23 @@
-import React, { useContext } from 'react';
-import { Transacao } from './Transacao';
+import React, { useContext } from "react";
+import { Transacao } from "./Transacao";
 
-import { GlobalContext } from '../context/GlobalState';
+import { GlobalContext } from "../context/GlobalState";
 
 export const ListadeTransacoes = () => {
+  const { transacoes } = useContext(GlobalContext);
 
-    const { transacoes } = useContext(GlobalContext);
+  console.log(transacoes);
 
-    return (
-        <>
-          <h3 className="text-center">Histórico de Transações </h3>
-          <ul id="list" className="list pb-4">
-            {transacoes.map(transacoes => (<Transacao key={transacoes.id} transacoes={transacoes} />))}    
-          </ul>  
-        </>
-    )
-}
+  return (
+    <>
+      {transacoes.length > 0 && (
+        <h3>Histórico de Transações</h3>
+      )}
+      <ul id="list" className="list pb-4">
+        {transacoes.map((transacoes) => (
+          <Transacao key={transacoes.id} transacoes={transacoes} />
+        ))}
+      </ul>
+    </>
+  );
+};
